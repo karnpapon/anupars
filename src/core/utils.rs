8f5408feba06d39a55e33f6e642f465ba79c74fe
Karnpapon Boonput <karnpapon@gmail.com>
2024-08-19 01:56:15 +0700

    handle empty text in build_doc_string

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -4,6 +4,10 @@ use super::config;
 pub fn build_doc_string(src: &config::StaticStrStr) -> String {
   let mut doc_str = String::new();
   for (command, desc) in src.iter() {
+    if command.is_empty() && desc.is_empty() {
+      doc_str.push('\n');
+      continue;
+    }
     doc_str.push_str(format!("{}: {}\n", command, desc).as_str());
   }
 
